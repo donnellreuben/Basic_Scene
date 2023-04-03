fe3b2e023382e7a635fea39c64e28796585b20a1
Donnell Reuben <donnell.reuben@icloud.com>
2023-04-03 16:13:35 -0500

    Added mesh to the scene

--- script.js
+++ script.js
@@ -5,3 +5,4 @@ const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 'red' });
 const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
