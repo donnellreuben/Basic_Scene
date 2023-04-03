a9b8d7aee0e611dec4e53775ec5e33252730c814
Donnell Reuben <donnell.reuben@icloud.com>
2023-04-03 16:55:44 -0500

    Created Perspective Camera

--- script.js
+++ script.js
@@ -6,3 +6,6 @@ const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 'red' });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
+
+// Camera
+const camera = new THREE.PerspectiveCamera();
