e9b10f5bd513c2287b4424c49fb6fbfd3b7125c0
Donnell Reuben <donnell.reuben@icloud.com>
2023-04-03 17:40:13 -0500

    Added camera positions and edited them to make sure it is a cube, and it is but right now all you will see is a 2D red cube

--- script.js
+++ script.js
@@ -15,6 +15,9 @@ const sizes = {
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+camera.position.x = 0;
+camera.position.y = 0;
 scene.add(camera);
 
 // Renderer
