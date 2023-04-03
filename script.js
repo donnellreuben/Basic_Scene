80bb73d5dc3cf97fbaa77d0e4a6570ff877777b2
Donnell Reuben <donnell.reuben@icloud.com>
2023-04-03 16:12:46 -0500

    Created Mesh and added geometry and material to it

--- script.js
+++ script.js
@@ -4,3 +4,4 @@ const scene = new THREE.Scene();
 // Red Cube
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 'red' });
+const mesh = new THREE.Mesh(geometry, material);
